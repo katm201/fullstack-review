47a3ce0a4582f17a91c662b719b73435be52e34c
Katherine Manning <katm201@gmail.com>
2017-11-08 15:28:02 -0800

    Add a findAllByLogin method on Repo instances and reorganize into the on open callback

--- database/index.js
+++ database/index.js
@@ -1,31 +1,32 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-  id: Number,
-  name: String,
-  url: String,
-  forks_count: Number,
-  login: String
-});
-
-// can put methods on the repoSchema
-
-let Repo = mongoose.model('Repo', repoSchema);
-
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
-
+mongoose.connect('mongodb://localhost:27017/fetcher');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.on('open', function() {
   // we're connected! all schema stuff goes in here
-  // each repo (row in a SQL db), is an instance of Repo
+  // each repo (row in a SQL db, document in Mongo), is an instance of Repo model
+
+  let repoSchema = mongoose.Schema({
+    id: Number,
+    name: String,
+    url: String,
+    forks_count: Number,
+    login: String
+  });
+
+  let Repo = mongoose.model('Repo', repoSchema);
+
+  repoSchema.methods.findAllByLogin = function(callback) {
+    return this.model('Repo').find({login: this.login}, callback);
+  };
+
+  let save = (/* TODO */) => {
+    // TODO: Your code here
+    // This function should save a repo or repos to
+    // the MongoDB
+
+  };
 });
 
 module.exports.save = save;
