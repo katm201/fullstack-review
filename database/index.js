b7cd548b9e8c47d7d1a766578a37795dc53b959c
Katherine Manning <katm201@gmail.com>
2017-11-08 15:15:08 -0800

    Build out the repoSchema

--- database/index.js
+++ database/index.js
@@ -3,7 +3,14 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  url: String,
+  forks_count: Number,
+  login: String
 });
+
+// can put methods on the repoSchema
 
 let Repo = mongoose.model('Repo', repoSchema);
 
@@ -12,5 +19,13 @@ let save = (/* TODO */) => {
   // This function should save a repo or repos to
   // the MongoDB
 }
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.on('open', function() {
+  // we're connected! all schema stuff goes in here
+  // each repo (row in a SQL db), is an instance of Repo
+});
 
 module.exports.save = save;
